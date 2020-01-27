4a15869473ff67a6674919a04abf7a8244e98fc0
Gareth Down <g@garethdown.com>
2020-01-27 14:25:59 +0000

    add documentation example

--- test/documentation.spec.js
+++ test/documentation.spec.js
@@ -1,13 +1,18 @@
 const MockServerClient = require('../index');
-const { path } = require('../queries');
+const { path, method, get } = require('../queries');
 const responses = require('../responses');
+
+// todo: move these out to a separate repo so the library can be consumed properly, as it would be in real life
+//
+// todo: would it make more sense to say responses.ok.json({ key: val })?, responses.ok.text('hello')?
 
 describe('tests to make sure the documentation is correct', () => {
   it('simple example', () => {
-    const client = new MockServerClient('http://localhost:8080');
+    const client = new MocksomeClient('http://localhost:8080');
 
     const run = async () => {
       await client.expectation()
+                  .with(method('GET'))
                   .with(path('/somepath'))
                   .response(responses.json.ok({ hello: 'goodbye' }))
                   .create();
@@ -15,4 +20,17 @@ describe('tests to make sure the documentation is correct', () => {
 
     run();
   });
+
+  it('convenience method example', async () => {
+    const client = new MocksomeClient('http://localhost:8080');
+
+    const run = async () => {
+      await client.expectation()
+                  .with(get('/somepath'))
+                  .response(responses.json.ok({ hello: 'goodbye' }))
+                  .create();
+    }
+
+    run();
+  })
 });
